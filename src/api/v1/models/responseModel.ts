dd5c4db07bd87cef9cf6f467d09ab5adca3dcd6c
rrc_f2024_aum <amistry@academic.rrc.ca>
2026-02-24 08:21:41 -0600

    feat: create responseModel file that has success and error response object.

---
+++ src/api/v1/models/responseModel.ts
@@ -0,0 +1,25 @@
+export interface ApiResponse<T> {
+    status: string,
+    data?: T,
+    message?: string,
+    error?: string,
+    code?: string
+}
+
+export const successResponse = <T>(
+    data?: T,
+    message?: string
+): ApiResponse<T> => ({
+    status: "success",
+    data,
+    message
+});
+
+export const errorResponse = <T>(
+    error: string,
+    code?: string
+): ApiResponse<null> => ({
+    status: "error",
+    error,
+    code
+});
