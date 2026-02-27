1d690ef4942dbf244565b2dbab0d9b7f9689b578
rrc_f2024_aum <amistry@academic.rrc.ca>
2026-02-27 00:02:05 -0600

    feat: added getAllEvents function in the file.

--- src/api/v1/services/eventService.ts
+++ src/api/v1/services/eventService.ts
@@ -3,6 +3,7 @@ import * as firestoreRepository from "../repositories/firestoreRepository";
 
 const COLLECTION_NAME = "events";
 
+// create event
 export const createEvent = async(eventData: Partial<Event>): Promise<string> => {
    
     try {
@@ -19,3 +20,28 @@ export const createEvent = async(eventData: Partial<Event>): Promise<string> => 
         throw new Error(`Failed to create event: ${errorMessage}`);
     }
 }
+
+// get all events
+export const getAllEvents = async(): Promise<Event[]> => {
+    try {
+        const snapshot = await firestoreRepository.getDocuments(COLLECTION_NAME);
+
+        const events: Event[] = [];
+        snapshot.forEach((doc) => {
+            events.push({
+                id: doc.id,
+                ...doc.data()
+            } as Event);
+        });
+
+        return events;
+    } catch (error: unknown) {
+        const errorMessage = 
+            error instanceof Error ? error.message: "Unknown error";
+        throw new Error(`Failed to fetch events: ${errorMessage}`);
+    }
+
+}
+// get event by ID
+// update event by ID
+// delete event by ID
