4dd5ccb4f826af8256e04202c684be549b608052
rrc_f2024_aum <amistry@academic.rrc.ca>
2026-02-28 20:33:55 -0600

    fix: modified health check endpoint route.

--- src/api/v1/routes/eventRoutes.ts
+++ src/api/v1/routes/eventRoutes.ts
@@ -2,39 +2,40 @@ import express from "express";
 import { validateRequest } from "../middleware/validate";
 import * as eventController from "../controllers/eventController";
 import { eventSchemas } from "../validations/eventSchemas";
-import { getHealthStatus } from "../services/eventService";
+//import { getHealthStatus } from "../services/eventService";
 
 const router = express.Router();
 
 // GET - health check
-router.get("/health", getHealthStatus)
+router.get("/health", 
+    eventController.checkHealth)
 
 // GET - all events
-router.get("/",
+router.get("/events",
     validateRequest(eventSchemas.list),
     eventController.getAllEventsHandler
 );
 
 //GET - event by id
-router.get("/:id",
+router.get("/events/:id",
     validateRequest(eventSchemas.getById),
     eventController.getEventByIdHandler
 );
 
 //POST - create event
-router.post("/",
+router.post("/events",
     validateRequest(eventSchemas.create),
     eventController.createEventHandler
 );
 
 // PUT - update event 
-router.put("/:id",
+router.put("/events/:id",
     validateRequest(eventSchemas.update),
     eventController.updateEventHandler
 );
 
 // DELETE - remove event
-router.get("/:id",
+router.delete("/events/:id",
     validateRequest(eventSchemas.getById),
     eventController.deleteEventHandler
 );
