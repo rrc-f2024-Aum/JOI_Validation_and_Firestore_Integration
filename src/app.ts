4771b533052a5f2e1d91403a8df6c3d3e444ff7f
rrc_f2024_aum <amistry@academic.rrc.ca>
2026-03-28 14:02:31 -0500

    feat: updated application file with cors configuration.

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import dotenv from "dotenv";
 dotenv.config();
-
-import { getHelmetConfig } from "../config/helmetConfig";
 import express from "express";
+import cors from "cors";
+import { getHelmetConfig } from "../config/helmetConfig";
+import { getCorsOptions } from "../config/corsConfig";
 import morgan from "morgan";
 import eventRoutes from "./api/v1/routes/eventRoutes";
 
@@ -13,6 +14,9 @@ import eventRoutes from "./api/v1/routes/eventRoutes";
 const app = express();
 
 app.use(getHelmetConfig());
+
+app.use(cors(getCorsOptions()));
+
 app.use(morgan("combined"));
 app.use(express.json());
 
