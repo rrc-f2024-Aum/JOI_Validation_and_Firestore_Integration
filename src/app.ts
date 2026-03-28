31c39f44b8a8ffff8c1c903d8415f02dbee96f0a
rrc_f2024_aum <amistry@academic.rrc.ca>
2026-03-28 13:05:13 -0500

    feat: added helmet to application file.

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,18 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+import { getHelmetConfig } from "../config/helmetConfig";
 import express from "express";
 import morgan from "morgan";
 import eventRoutes from "./api/v1/routes/eventRoutes";
 
 
 
+
 // Initialize Express application
 const app = express();
 
+app.use(getHelmetConfig());
 app.use(morgan("combined"));
 app.use(express.json());
 
